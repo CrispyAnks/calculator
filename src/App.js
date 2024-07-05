9028484b78b2add056549bd8103e8563704f354e
makoto <ankscrisp0505@gmail.com>
2024-07-05 15:23:54 +0900

    first upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {React, useState} from 'react';
+import Screen from './components/Screen';
+import History from './components/History';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+  const [history, setHistory] = useState([]);
+
+  const addHistory = (value) => {
+    setHistory([...history, value])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='app-container'>
+      <Row>
+      <Col className='app-row' sm={8}><Screen addHistory={addHistory}/></Col>
+      <Col className='app-row' sm={4}><History history = {history} /></Col> 
+      </Row>
+    </Container>
   );
 }
 
